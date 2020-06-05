95ec783fad89b0af94b607cace92b051f48288b3
danney-chun <63285271+danney-chun@users.noreply.github.com>
2020-06-05 15:59:11 +0900

    [CALLS-534]widget Sample App Design Improvements
    1. fixed disign issue
    2. add application information menu

--- envs.js
+++ envs.js
@@ -1,7 +1,4 @@
-// export const TEST_APP_ID = BUILD_APP_ID || undefined;
-// export const TEST_APP_ID = "6C53468D-A6AB-4160-B586-7F5DB1DBF1D4";
-export const TEST_APP_ID = "7768FD30-5639-48BC-8B25-F5211F3DFEBB";
-// export const TEST_APP_ID = "80ED633C-5301-4BEB-B1DC-0605A395DFAC";
+export const TEST_APP_ID = BUILD_APP_ID || undefined;
 export const USER_ID = undefined;
 export const ACCESS_TOKEN = undefined;
 export const IS_ACCESS_TOKEN_NEEDED = false;
